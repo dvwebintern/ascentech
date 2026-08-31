417ef77b75f22468b89c76ac081e787974a72d82
DV Web Intern <dvwebintern@gmail.com>
2026-08-31 17:36:03 +0530

    done

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,12 +11,10 @@
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
-    <link
-        rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
-    >
-<link rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+   <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
+>
     {{-- Alpine.js --}}
     <script
         defer
@@ -25,8 +23,7 @@
 <link rel="stylesheet" href="{{ asset('css/mobile-nav.css') }}">
 </head>
 
-<body class="font-sans text-gray-800 antialiased">
-
+<body class="text-gray-800 antialiased">
     <main>
         @yield('content')
     </main>
