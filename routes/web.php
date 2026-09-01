f59f38871729f3077ea4530b5e0c95af89c27775
DV Web Intern <dvwebintern@gmail.com>
2026-09-01 09:40:45 +0530

    Test Render Laravel route

--- routes/web.php
+++ routes/web.php
@@ -1,38 +1,85 @@
+
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Storage;
+
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\CareerController;
+
+
+/*
+|--------------------------------------------------------------------------
+| Test Route
+|--------------------------------------------------------------------------
+*/
 
 Route::get('/test-render', function () {
     return 'RENDER LARAVEL IS WORKING';
 });
 
-// your existing routes below...
 
-
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\PageController;
+/*
+|--------------------------------------------------------------------------
+| Main Pages
+|--------------------------------------------------------------------------
+*/
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
-Route::get('/about-us', [PageController::class, 'about'])->name('about');
-Route::get('/services', [PageController::class, 'services'])->name('services');
-Route::get('/nagarkaryavali', [PageController::class, 'nagarkaryavali'])->name('products.nagarkaryavali');
-Route::get('/banking-solutions', [PageController::class, 'banking'])->name('products.banking');
-Route::get('/events', [PageController::class, 'events'])->name('events');
-Route::get('/awards', [PageController::class, 'awards'])->name('awards');
-Route::get('/news-media', [PageController::class, 'news'])->name('news');
-Route::get('/careers', [PageController::class, 'careers'])->name('careers');
-Route::get('/contact-us', [PageController::class, 'contact'])->name('contact');
-Route::post('/contact-us', [PageController::class, 'contactSubmit'])->name('contact.submit');
 
-use App\Http\Controllers\CareerController;
+Route::get('/about-us', [PageController::class, 'about'])->name('about');
+
+Route::get('/services', [PageController::class, 'services'])->name('services');
+
+Route::get('/nagarkaryavali', [PageController::class, 'nagarkaryavali'])
+    ->name('products.nagarkaryavali');
+
+Route::get('/banking-solutions', [PageController::class, 'banking'])
+    ->name('products.banking');
+
+Route::get('/events', [PageController::class, 'events'])
+    ->name('events');
+
+Route::get('/awards', [PageController::class, 'awards'])
+    ->name('awards');
+
+Route::get('/news-media', [PageController::class, 'news'])
+    ->name('news');
+
+Route::get('/careers', [PageController::class, 'careers'])
+    ->name('careers');
+
+Route::get('/contact-us', [PageController::class, 'contact'])
+    ->name('contact');
+
+
+/*
+|--------------------------------------------------------------------------
+| Contact Form
+|--------------------------------------------------------------------------
+*/
+
+Route::post('/contact-us', [PageController::class, 'contactSubmit'])
+    ->name('contact.submit');
+
+
+/*
+|--------------------------------------------------------------------------
+| Career Form
+|--------------------------------------------------------------------------
+*/
 
 Route::post('/career/submit', [CareerController::class, 'submit'])
     ->name('career.submit');
 
-  use Illuminate\Support\Facades\Storage;
-  
+
+/*
+|--------------------------------------------------------------------------
+| Career CV Download
+|--------------------------------------------------------------------------
+*/
+
 Route::get('/careers/cv/{filename}', function ($filename) {
 
     $path = 'career-cvs/' . $filename;
@@ -54,5 +101,13 @@ Route::get('/careers/cv/{filename}', function ($filename) {
 
 })->where('filename', '.*')->name('career.cv');
 
+
+/*
+|--------------------------------------------------------------------------
+| Schedule Call Form
+|--------------------------------------------------------------------------
+*/
+
 Route::post('/schedule-submit', [PageController::class, 'scheduleSubmit'])
     ->name('schedule.submit');
+
